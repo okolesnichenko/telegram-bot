fbd03a0e992610e1d99d78bcebdb60ea1db152b0
worldintro <worldintro>
2019-03-04 18:37:58 +0300

    tbot v0.1

--- tbot.py
+++ tbot.py
@@ -66,11 +66,14 @@ def main():
             last_chat_text = last_update['message']['text']
             last_chat_id = last_update['message']['chat']['id']
             last_chat_name = last_update['message']['chat']['first_name']
+
             if last_chat_text.lower() in hi_text:
                 greet_bot.send_message(last_chat_id, "Привет, друг {}".format(last_chat_name))
             if last_chat_text.lower() in time_text:
                 greet_bot.send_message(last_chat_id,
                                     "Сегодня {today}, время {hour}:{minute}".format(today=time["today"], hour=time["hour"], minute=time["minute"]))
+            if last_chat_text.lower() in photo_text:
+                greet_bot.send_photo(last_chat_id, 'AgADAgAD-qkxG2Gq6Uu-1YHQN9kGXs5LXw8ABCO9hIPKcS1-wV8CAAEC')
 
             new_offset = last_update_id+1
 
