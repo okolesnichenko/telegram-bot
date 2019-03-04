988799d66ea5c8438f9137ae79712b2e5062de34
worldintro <worldintro>
2019-03-04 15:59:23 +0300

    tbot v0.1

--- tbot.py
+++ tbot.py
@@ -20,14 +20,14 @@ class BotHandler:
         if len(result)>0:
             last_update = result[-1]
         else:
-            last_update = result[len(result)]
+            last_update = None
         return last_update
 
     def get_chat_id(update):
         chat_id = update['message']['chat']['id']
         return chat_id
 
-    def send_mess(self, chat, text):
+    def send_message(self, chat, text):
         params = {'chat_id': chat, 'text':text}
         method = 'sendMessage'
         response = requests.post(self.api_url + method, data=params)
@@ -49,17 +49,17 @@ def main():
         greet_bot.get_updates_json(new_offset)
 
         last_update = greet_bot.get_last_update()
-
-        last_update_id = last_update['update_id']
-        last_chat_text = last_update['message']['text']
-        last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['chat']['first_name']
-        if last_chat_text.lower() in hi_text:
-            greet_bot.send_mess(last_chat_id, "Привет, друг {}".format(last_chat_name))
-        if last_chat_text.lower() in time_text:
-            greet_bot.send_mess(last_chat_id,
-                                "Сегодня {today}, время {hour}:{minute}".format(today=today, hour=hour, minute=minute))
-        new_offset = last_update_id+1
+        if(last_update):
+            last_update_id = last_update['update_id']
+            last_chat_text = last_update['message']['text']
+            last_chat_id = last_update['message']['chat']['id']
+            last_chat_name = last_update['message']['chat']['first_name']
+            if last_chat_text.lower() in hi_text:
+                greet_bot.send_message(last_chat_id, "Привет, друг {}".format(last_chat_name))
+            if last_chat_text.lower() in time_text:
+                greet_bot.send_message(last_chat_id,
+                                    "Сегодня {today}, время {hour}:{minute}".format(today=today, hour=hour, minute=minute))
+            new_offset = last_update_id+1
 
 if __name__ == '__main__':
     try:
