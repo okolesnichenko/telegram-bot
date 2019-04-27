78873e542d608bc0d07c6d57495cb2aa33a9a507
worldintro <worldintro>
2019-04-27 18:15:01 +0300

    recog voice

--- context.py
+++ context.py
@@ -1,11 +1,9 @@
 import speech_recognition as sr
 
 def recognize(file):
-    with sr.AudioFile(file) as source:
-        r = sr.Recognizer()
-        audio = r.record(source)
+
     try:
-        usertext = r.recognize_google(audio, language="ru_RU").lower()
+        usertext = r.recognize_google(file, language="ru_RU").lower()
         print(format(usertext))
     except sr.UnknownValueError:
         print("Скаже еще")
